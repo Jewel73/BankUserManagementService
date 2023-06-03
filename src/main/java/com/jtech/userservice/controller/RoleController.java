6c9f3dbc02488a339f19e2820ec8859353a02c29
Md Jewel <46159821+Jewel73@users.noreply.github.com>
2023-06-04 00:02:59 +0600

    Update RoleController.java

--- src/main/java/com/jtech/userservice/controller/RoleController.java
+++ src/main/java/com/jtech/userservice/controller/RoleController.java
@@ -1,5 +1,15 @@
 package com.jtech.userservice.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 public class RoleController {
-
+  
+    @GetMapping("/")
+      public String greeting() {
+        return "User added successfully............";
+      }
 }
+
+
+	
